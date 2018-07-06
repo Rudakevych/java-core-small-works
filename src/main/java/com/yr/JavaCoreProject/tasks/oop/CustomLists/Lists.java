14a349a3950767f3fed642d6e3c05a47e9fbbaf3
Rudakevych <yurij.rudakevych@gmail.com>
2018-07-07 01:04:22 +0300

    added custom lists task

---
+++ src/main/java/com/yr/JavaCoreProject/tasks/oop/CustomLists/Lists.java
@@ -0,0 +1,13 @@
+package com.yr.JavaCoreProject.tasks.oop.CustomLists;
+
+public interface Lists {
+
+    boolean add();
+    int size();
+    boolean remove(Object o);
+    boolean addAll();
+    void clear();
+    Object get(int index);
+    Object set(int index, Object element);
+
+}
